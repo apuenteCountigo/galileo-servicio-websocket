cc528651e133ea2ab1e253fd56cdcbc379cde61c
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-10 16:28:53 -0400

    WS INIT-05

--- servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/config/WebSocketConfig.java
+++ servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/config/WebSocketConfig.java
@@ -1,5 +1,10 @@
 package com.galileo.cu.servicio_websocket.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
--- servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/controllers/WebSocketController.java
+++ servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/controllers/WebSocketController.java
@@ -1,5 +1,8 @@
 package com.galileo.cu.servicio_websocket.controllers;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
 @Controller
 public class WebSocketController {
     @MessageMapping("/sendError")
