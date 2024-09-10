a26eb8c3140ddd9426e71c721c2ebff2ca822cff
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-10 15:58:18 -0400

    WS INIT-04

--- servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/ServicioWebsocketApplication.java
+++ servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/ServicioWebsocketApplication.java
@@ -14,6 +14,6 @@ public class ServicioWebsocketApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Websocket V-24009101539");
+		System.out.println("Websocket V-24009101558");
 	}
 }
