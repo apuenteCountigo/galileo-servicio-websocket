e6101b44978fd7e0a75f8c9a24afd4491d891f33
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-10 14:40:03 -0400

    INIT-01

--- servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/ServicioWebsocketApplication.java
+++ servicio-websocket/src/main/java/com/galileo/cu/servicio_websocket/ServicioWebsocketApplication.java
@@ -3,10 +3,8 @@ package com.galileo.cu.servicio_websocket;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({ "com.galileo.cu.commons.models" })
 public class ServicioWebsocketApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
